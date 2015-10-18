8e4fac765d9bc5b05fd399f3d171b72ca5135b24
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-10-18 15:28:46 +0200

    Unstable version, not completed

---
+++ MusicStore/MusicStore.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,32 @@
+﻿using MusicStore.Web.Models;
+using MusicStore.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Web.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        //
+        // GET: /ShoppingCart/
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Setup our ViewModel
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems=cart.GetCartItems(),
+                CartTotal=cart.GetTotal()
+            };
+
+            // Return the view
+            return View(viewModel);
+        }
+	}
+}
---
+++ MusicStore/MusicStore.Web/Models/ShoppingCart.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Web.Models
+{
+    public partial class ShoppingCart
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        string ShoppingCartId { get; set; }
+
+        public const string CartSessionKey = "CartId";
+
+        public static ShoppingCart GetCart(HttpContextBase context)
+        {
+            var cart = new ShoppingCart();
+            cart.ShoppingCartId = cart.GetCartId(context);
+            return cart;
+        }
+
+        // Helper method to simplify shopping cart calls
+        //public static ShoppingCart GetCart(Controller controller)
+
+        //public void AddToCart(Album album)
+
+        //public int RemoveFromCart(int id)
+
+        //public void EmptyCart()
+
+        public List<Cart> GetCartItems()
+        {
+            return storeDB.Carts.Where(cart => cart.CartId == ShoppingCartId).ToList();
+        }
+
+        //public int GetCount()
+
+        public decimal GetTotal()
+        {
+            // Multiply album price by count of that album to get
+            // the current price for each of those albums in the cart
+            // sum all album price totals to get the cart total
+
+            decimal? total = (from cartItems in storeDB.Carts
+                              where cartItems.CartId == ShoppingCartId
+                              select (int?)cartItems.Count * cartItems.Album.Price).Sum();
+            return total ?? decimal.Zero;
+        }
+
+        //public int CreateOrder()
+
+        // We're using HttpContextBase to allow access to cookies.
+        public string GetCartId(HttpContextBase context)
+        {
+            if (context.Session[CartSessionKey] == null)
+            {
+                if (!string.IsNullOrWhiteSpace(context.User.Identity.Name))
+                {
+                    context.Session[CartSessionKey] = context.User.Identity.Name;
+                }
+                else
+                {
+                    // Generate a new random GUID using System.Guid class
+                    Guid tempCartId = Guid.NewGuid();
+
+                    // Send tempCartId back to a client as a cookie
+                    context.Session[CartSessionKey] = tempCartId.ToString();
+                }
+            }
+
+            return context.Session[CartSessionKey].ToString();
+        }
+
+        // When a user has logged in, migrate their shopping cart to
+        // be associated with their username
+        //public void MigrateCart(string userName)
+    }
+}
